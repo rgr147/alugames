db2927660cd117b0115a953208d0716b082ce2b1
Roger Alves <rgr_147@hotmail.com>
2024-03-29 16:33:05 -0300

    projeto inicial

---
+++ js/app.js
@@ -0,0 +1,9 @@
+function alterarStatus(item){
+    jogo = `game-${item}`
+    //deixando todo o elemento do jogo na sombreado
+    document.getElementById(jogo).classList.add('dashboard__item__img--rented');
+
+    
+
+
+}
