473a191f411a614c554ec7223da57bd3a5092994
rgr147 <139695514+rgr147@users.noreply.github.com>
2024-03-30 17:15:31 -0300

    Jogo refatorado e implementado as funções:  'devolverJogo' e 'alugarJogo'

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
-let jogosAlugados = 1;
+let jogosAlugados = 0;
 
-function contarEExibirJogosAlugados(){
+function contarEExibirJogosAlugados() {
     console.log(`Total de jogos alugados: ${jogosAlugados}`);
 }
 
@@ -12,24 +12,33 @@ function alterarStatus(numero) {
 
     if(imagem.classList.contains('dashboard__item__img--rented')) {
         let confirmacao = parseInt(prompt(`Devolver: ${nome.textContent}? Digite: 1(SIM) 2(NAO)`));
-        if(confirmacao){
-            imagem.classList.remove('dashboard__item__img--rented');
-            botao.classList.remove('dashboard__item__button--return');
-            botao.textContent='Alugar'
-            alert(`${nome.textContent}: DEVOLVIDO COM SUCESSO`);
-            jogosAlugados--;
+        
+        if(confirmacao == 1){
+            devolverJogo(imagem,botao,nome);
         } else {
-            alert('Devolução cancelada!')
+            alert('Devolução cancelada!');
             return;
         }
     } else {
-        imagem.classList.add('dashboard__item__img--rented');
-        botao.classList.add('dashboard__item__button--return');
-        botao.textContent='Devolver'
-        alert(`${nome.textContent}: ALUGADO COM SUCESSO`);
-        jogosAlugados++;
+        alugarJogo(imagem,botao,nome);
     }
     contarEExibirJogosAlugados();
+}
+
+function devolverJogo(imagem,botao,nome) {
+    imagem.classList.remove('dashboard__item__img--rented');
+    botao.classList.remove('dashboard__item__button--return');
+    botao.textContent='Alugar';
+    alert(`${nome.textContent}: DEVOLVIDO COM SUCESSO`);
+    jogosAlugados--;
+}
+
+function alugarJogo(imagem,botao,nome) {
+    imagem.classList.add('dashboard__item__img--rented');
+    botao.classList.add('dashboard__item__button--return');
+    botao.textContent='Devolver';
+    alert(`${nome.textContent}: ALUGADO COM SUCESSO`);
+    jogosAlugados++;
 }
 
 //inicia a contagem considerando que os jogos já começam alugados
@@ -37,3 +46,4 @@ document.addEventListener('DOMContentLoaded', function() {
     jogosAlugados = document.querySelectorAll('.dashboard__item__img--rented').length;
     contarEExibirJogosAlugados();
 });
+
